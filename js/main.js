5f2bec7d7f890647c56097211ef08771d78a0480
Romain Tello <romain.tello@gmail.com>
2020-10-01 09:45:35 +0200

    Solve step 2

--- js/main.js
+++ js/main.js
@@ -23,3 +23,23 @@ for (let element of $('#question-form label')) {
   count += 1;
 }
 */
+
+// Initialise l'index de la question actuelle à zéro
+let currentQuestionId = 0;
+// Associe une action au fait de valider le formulaire
+$('#question-form').submit( function(event) {
+  // Empêche le rechargement de la page
+  event.preventDefault();
+  // Augmente l'index de la question actuelle de 1
+  currentQuestionId += 1;
+  // Charge la question actuelle dans le DOM
+  // Change le numéro de la question
+  $('#question-id').text(currentQuestionId + 1);
+  // Change le texte de la question
+  $('#current-question-text').text(questionData[currentQuestionId].text);
+  // Change le texte de chaque réponse
+  $('#answer1-caption').text(questionData[currentQuestionId].answer1);
+  $('#answer2-caption').text(questionData[currentQuestionId].answer2);
+  $('#answer3-caption').text(questionData[currentQuestionId].answer3);
+  $('#answer4-caption').text(questionData[currentQuestionId].answer4);
+});
